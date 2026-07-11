cad91b5be9db400f3f4f9362c0c23a4ed151375c
rajat pathak <pathakrajat463@gmail.com>
2026-07-11 20:09:20 +0530

    Configure Neon DB connection and env setup

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -7,7 +7,7 @@ from config import Settings as settings
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-PG_DB_URL = f"postgresql://{settings.PG_DB_USER}:{quote_plus(settings.PG_DB_PASSWORD)}@{settings.PG_DB_SERVER}:{settings.PG_DB_PORT}/{settings.PG_DATABASE}"
+PG_DB_URL = f"postgresql://{settings.PG_DB_USER}:{quote_plus(settings.PG_DB_PASSWORD)}@{settings.PG_DB_SERVER}:{settings.PG_DB_PORT}/{settings.PG_DATABASE}?sslmode=require"
 class Database: 
     def __init__(self):
         self.database_url = PG_DB_URL
