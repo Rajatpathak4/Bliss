d7f67d7ddb5e8a0c4395858018a036a170ada1eb
rajat pathak <pathakrajat463@gmail.com>
2026-07-12 10:37:37 +0530

    Fix Neon pooled connection

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,43 +1,61 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
 from contextlib import contextmanager
 from urllib.parse import quote_plus
 
 from config import Settings as settings
-from sqlalchemy import MetaData
-from sqlalchemy.ext.declarative import declarative_base
 
-PG_DB_URL = f"postgresql://{settings.PG_DB_USER}:{quote_plus(settings.PG_DB_PASSWORD)}@{settings.PG_DB_SERVER}:{settings.PG_DB_PORT}/{settings.PG_DATABASE}?sslmode=require"
-print('PG_DB_URL:', PG_DB_URL)
-class Database: 
+PG_DB_URL = (
+    f"postgresql://{settings.PG_DB_USER}:"
+    f"{quote_plus(settings.PG_DB_PASSWORD)}@"
+    f"{settings.PG_DB_SERVER}:"
+    f"{settings.PG_DB_PORT}/"
+    f"{settings.PG_DATABASE}?sslmode=require"
+)
+
+print("PG_DB_URL:", PG_DB_URL)
+
+class Database:
     def __init__(self):
         self.database_url = PG_DB_URL
-        self.engine = create_engine(PG_DB_URL,connect_args={"options": "-c timezone=Asia/Kolkata"},echo=False)
+        self.engine = create_engine(
+            PG_DB_URL,
+            echo=False
+        )
         self.session = sessionmaker(bind=self.engine)
-      
+
     @contextmanager
     def connect(self):
         session = self.session()
         try:
             yield session
             session.commit()
-        except Exception as e:
+        except Exception:
             session.rollback()
-            raise e
+            raise
         finally:
             session.close()
+
 
 def get_db():
     with Database().connect() as db_session:
         yield db_session
 
-Base = declarative_base(metadata=MetaData(schema='public'))
 
-#-------- SECOND DB FOR TRANSACTION ---------------------------------------
+Base = declarative_base(metadata=MetaData(schema="public"))
 
-engine = create_engine(PG_DB_URL,connect_args={"options": "-c timezone=Asia/Kolkata"},echo=False)
+# -------- SECOND DB FOR TRANSACTION ---------------------------------------
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    PG_DB_URL,
+    echo=False
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 
 def get_transaction_db():
