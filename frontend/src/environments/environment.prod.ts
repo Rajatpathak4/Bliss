67dcbce560d70d9d20ad3c2b9c5757db910f4526
rajat pathak <pathakrajat463@gmail.com>
2026-07-12 10:48:18 +0530

    Switch to psycopg2-binary for Vercel compatibility

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://bliss-backend-37st5ys4i-rajatpathak5.vercel.app/'
+  apiBaseUrl: 'https://bliss-backend-37st5ys4i-rajatpathak5.vercel.app'
 };
