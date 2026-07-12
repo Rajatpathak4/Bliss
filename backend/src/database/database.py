1b30599acd4633d6b78e3d06193efca66f98072f
rajat.pathak <rajat.pathak@sharajman.com>
2026-07-12 23:00:05 +0530

    remove vercel folder

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -6,7 +6,7 @@ from config import Settings as settings
 from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-PG_DB_URL = f"postgresql://{settings.PG_DB_USER}:{quote_plus(settings.PG_DB_PASSWORD)}@{settings.PG_DB_SERVER}:{settings.PG_DB_PORT}/{settings.PG_DATABASE}?sslmode=require&channel_binding=require"
+PG_DB_URL = f"postgresql://{settings.PG_DB_USER}:{quote_plus(settings.PG_DB_PASSWORD)}@{settings.PG_DB_SERVER}:{settings.PG_DB_PORT}/{settings.PG_DATABASE}"
 
 print('PG_DB_URL:', PG_DB_URL)
 
