a791dc638f672709de62654039c84d157317ed82
Rajat pathak <161803780+Rajatpathak4@users.noreply.github.com>
2026-07-27 12:58:37 +0530

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -31,14 +31,6 @@ app = FastAPI(
     swagger_ui_parameters={"defaultModelsExpandDepth": -1},
 )
 
-# ------------------ CORS ------------------
-
-# origins = [
-#     "http://localhost:4200",
-#     "https://bliss-dun-six.vercel.app",
-#     "https://bliss-vnts.vercel.app",
-    
-# ]
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
