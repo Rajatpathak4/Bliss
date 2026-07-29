9e6fd242303be890c27231e110e5b4fee4e1efe6
rajat pathak <pathakrajat463@gmail.com>
2026-07-29 20:29:19 +0530

    alter table uuser

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,34 +12,30 @@ import { AppComponent } from "./app.component";
 import { AuthInterceptor } from "./core/interceptors/auth.interceptor";
 import {
   GoogleLoginProvider,
-  SOCIAL_AUTH_CONFIG,
-  SocialAuthServiceConfig,
+  SocialLoginModule,
 } from "@abacritt/angularx-social-login";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SocialLoginModule.initialize({
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '332250492224-epf9vk7ta9jksrmcav1liglk4je0t0jh.apps.googleusercontent.com',
+            { oneTapEnabled: false, prompt: 'select_account' }
+          ),
+        },
+      ],
+    }),
+  ],
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    {
-      provide: SOCIAL_AUTH_CONFIG,
-      useValue: {
-        autoLogin: false,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              "332250492224-epf9vk7ta9jksrmcav1liglk4je0t0jh.apps.googleusercontent.com",
-              {
-                oneTapEnabled: false,
-                prompt: "select_account",
-              },
-            ),
-          },
-        ],
-      } as SocialAuthServiceConfig,
-    },
   ],
   bootstrap: [AppComponent],
 })
